b3e19a2aac296ce756d10b74489fc068b1de3264
Serkin Alexander <serkin.alexander@gmail.com>
2023-02-23 23:34:52 +0700

    v2. /words

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from flask_mysqldb import MySQL
 from flask import Flask, render_template, request, redirect, url_for
 import os
@@ -19,39 +20,90 @@ def create_app():
         cur.execute(
             """SELECT id, base FROM words WHERE hidden_at IS NULL AND is_active = 1 ORDER BY RAND() limit 1""")
         rv = cur.fetchall()
-        question = None
+        word = None
         if rv:
-            question = rv[0]
+            word = rv[0]
         cur.close()
-        return render_template("index.html", stat=_stat(), question=question)
+        return render_template("index.html", stat=_stat(), word=word)
+
+    @app.route("/words")
+    def words():
+        cur = mysql.connection.cursor()
+        show_all_words = request.args.get('show_all_words')
+        cur.execute(f"""
+SELECT
+    words.id,
+    base,
+    th,
+    is_active,
+    log.status,
+    COUNT(*) as total
+FROM
+    words
+LEFT JOIN log ON words.id = log.word_id
+WHERE
+    words.hidden_at IS NULL {"AND DATE(log.timestamp) = CURDATE()" if not show_all_words else ""}
+GROUP BY
+    log.status, base
+ORDER BY
+    is_active desc, base;""")
+        words = defaultdict(dict)
+        for word in cur.fetchall():
+            words[word["id"]] = dict(
+                th=word["th"], base=word["base"], fail=0, success=1, is_active=word["is_active"])
+            words[word["id"]][word["status"]] = word["total"]
+        cur.close()
+        return render_template("words.html", stat=_stat(), words=words)
 
     @app.route("/answer", methods=['POST', 'GET'])
     def answer():
-        result = "fail"
+        result = None
         form = request.form
-        word_id = form.get('word_id')
-        answer = form.get('answer')
+        word_id = form.get('word_id', request.args.get("word_id"))
+        user_answer = form.get('user_answer')
+
         cur = mysql.connection.cursor()
         cur.execute(
-            """SELECT base, th, spelling FROM words WHERE id=%s LIMIT 1""", (word_id,))
+            """SELECT  id, base, th, spelling FROM words WHERE id=%s LIMIT 1""", (word_id,))
         rv = cur.fetchall()
-        if answer and rv[0]["th"] == answer:
-            result = "success"
-            answer = None
-
-        if answer:
+        if user_answer:
+            if rv and rv[0]["th"] == user_answer:
+                result = "success"
+            else:
+                result = "fail"
+        if user_answer:
             cur.execute("""INSERT INTO log (word_id, status, answer) values(%s, %s, %s);""",
-                        (form.get('word_id'), result, answer))
+                        (form.get('word_id'), result, user_answer))
             mysql.connection.commit()
         cur.close()
-        return render_template("answer.html", stat=_stat(), result=result, spelling=rv[0]["spelling"], question=rv[0]["base"], answer=answer, word_id=word_id, correct_answer=rv[0]["th"])
+        return render_template("answer.html", stat=_stat(), result=result, word=rv[0], user_answer=user_answer)
 
-    @app.route("/never")
-    def never():
+    @app.route("/<word_id>/activate")
+    def activate(word_id):
         cur = mysql.connection.cursor()
 
-        cur.execute("""UPDATE words SET hidden_at = current_timestamp WHERE id = %s;""",
-                    (request.args.get("word_id"),))
+        cur.execute(
+            """UPDATE words SET is_active = 1 WHERE id = %s;""", (word_id,))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("words"))
+
+    @app.route("/<word_id>/deactivate")
+    def deactivate(word_id):
+        cur = mysql.connection.cursor()
+
+        cur.execute(
+            """UPDATE words SET is_active = 0 WHERE id = %s;""", (word_id,))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("words", show_all_words=1))
+
+    @app.route("/<word_id>/hide")
+    def never(word_id):
+        cur = mysql.connection.cursor()
+
+        cur.execute(
+            """UPDATE words SET hidden_at = current_timestamp WHERE id = %s;""", (word_id,))
         mysql.connection.commit()
         cur.close()
         return redirect(url_for("index"))
